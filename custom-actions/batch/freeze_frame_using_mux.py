a43991d340082b0bd2e1930c7c2ea7d3517c227b
fredwarren <derfwarren@hotmail.com>
2019-07-26 11:29:11 -0400

    Add custom action to freeze a frame using a MUX node

---
+++ custom-actions/batch/freeze_frame_using_mux.py
@@ -0,0 +1,66 @@
+"""Connect a MUX to a node and freeze the current frame"""
+
+def get_batch_custom_ui_actions():
+    """Return custom actions to execute on Batch objects."""
+
+    def get_context():
+        """Get the first available slot in a Batch Group's Context views list."""
+        import flame
+        contexts = flame.batch.contexts
+        for context in range(1, 11):
+            if contexts.has_key(context) is False:
+                return context
+        return 0
+
+    def scope_node(selection):
+        """Scope the custom action to the nodes residing in a Batch Schematic."""
+        import flame
+        for item in selection:
+            if isinstance(item, (flame.PyNode)):
+                return True
+        return False
+
+    def add_mux_and_freeze(selection):
+        """Add a MUX node and freeze the current frame"""
+        import flame
+
+        current = flame.batch.current_node.get_value()
+        time = flame.batch.current_frame
+
+        # Create the MUX node
+        mux = flame.batch.create_node("Mux")
+        mux.pos_x = current.pos_x + 200
+        mux.pos_y = current.pos_y
+        context_nb = get_context()
+        if context_nb is not 0:
+            mux.set_context(context_nb, "Result")
+        mux.range_active = True
+        mux.range_start = time
+        mux.range_end = time
+        mux.before_range = "Repeat First" 
+        mux.after_range = "Repeat Last"
+
+        # Connect the Result output socket to the MUX's Front socket
+        flame.batch.connect_nodes(current, "Default", mux, "Default")
+
+        # Connect the first Matte output socket to the MUX's Matte socket
+        socket_number = 0
+        for socket in current.output_sockets:
+            if socket_number > 0:
+                if "matte" in socket.lower() or "alpha" in socket.lower():
+                    flame.batch.connect_nodes(current, socket, mux, "Matte_0")
+                    break
+            socket_number = socket_number + 1
+
+    return [
+         {
+            "name": "Custom",
+            "actions": [
+                {
+                    "name": "Add Mux Node and Freeze",
+                    "isVisible": scope_node,
+                    "execute": add_mux_and_freeze
+                }
+            ]
+        }
+    ]
