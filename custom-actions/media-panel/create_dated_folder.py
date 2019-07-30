267c26548d056dd2f00eb1155acde70e7c872262
Andy Milkis <andy@lvly.tv>
2019-07-30 11:43:23 -0400

    Create create_dated_folder.py
    Script that creates a dated folder using the default Flame date pattern: YYYY_MM_DD

---
+++ custom-actions/media-panel/create_dated_folder.py
@@ -0,0 +1,34 @@
+#Creates a Dated Folder
+def get_media_panel_custom_ui_actions():
+
+    def scope_libraries(selection):
+        import flame
+        for item in selection:
+            if isinstance(item, (flame.PyLibrary)):
+                return True
+        return False
+
+    def dated_folder(selection):
+        import flame
+        import os
+        import datetime
+        date = datetime.date.today().strftime('%Y_%m_%d')
+
+        for item in selection:
+            datedFolder = item.create_folder(date)
+
+
+    return [
+        {
+            "name": "PYTHON: Library",
+            "actions": [
+                {
+                    "name": "Create Dated Folder",
+                    "isVisible": scope_libraries,
+                    "execute": dated_folder
+                    "minimumVersion": "2020"
+                }
+            ]
+        }
+
+    ]
